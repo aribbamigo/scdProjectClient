6f0bceca27044838bf7b474df704e416d86c658b
aribbamigo <pauldanvana@gmail.com>
2023-11-24 19:30:08 +0200

    first commit

---
+++ WinFormsApp1/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace WinFormsApp1
+{
+    internal class Employee
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+        public Department departmentId { get; set; }
+        public long managerId { get; set; }
+        public string email { get; set; }
+
+    }
+
+    internal class Department
+    {
+        private long id { get; set; }
+
+        private string description { get; set; }
+
+        private long parentID { get; set; }
+    }
+}
